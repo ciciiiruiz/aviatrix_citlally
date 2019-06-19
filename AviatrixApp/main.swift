6f04b97d585d6cf310df35434dc2ecf0296b59d7
Citlally Ruiz <ciciiiruiz@gmail.com>
2019-06-19 10:46:33 -0500

    Complete Iteration 0

--- AviatrixApp/main.swift
+++ AviatrixApp/main.swift
@@ -74,9 +74,12 @@ func fuelCheck(myPlane : Aviatrix, destination : String) -> Bool {
     return true
 }
 
-var plane = Aviatrix()
+var plane = Aviatrix(authorName : "Cici")
 
-print("Welcome to the Aviatrix Flight System by _________")
+
+
+print("Welcome to the Aviatrix Flight System by \(plane.author)")
+
 plane.start()
 
 print("You're currently in _________")
@@ -109,4 +112,4 @@ while command != "q" {
 }
 
 print(" ")
-print("Thanks for flying with _________ airline!")
+print("Thanks for flying with \(plane.author)'s airline!")
